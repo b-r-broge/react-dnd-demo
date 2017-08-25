a7741f42f33ebe40366c610430319141898ddd84
Brad Broge <brbroge@gmail.com>
2017-08-25 00:28:35 -0400

    adding a square

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Knight from './components/knight'
+import Knight from './components/knight';
+import Square from './components/square';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Knight />
+        <Square black>
+          <Knight />
+        </Square>
       </div>
     );
   }
